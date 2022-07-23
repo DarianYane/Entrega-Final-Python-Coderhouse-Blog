78753ec8e9378aeac9c22bb08345d01eaaf220d8
Darian Yane <darianyane@yahoo.com.ar>
2022-07-23 03:11:55 -0300

    BarraBusqueda

--- Posteos/views.py
+++ Posteos/views.py
@@ -1,15 +1,21 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from Posteos.models import Entrada
-from Posteos.forms import EntradaForm
+from Posteos.forms import EntradaForm, BusquedaEntrada
+from django.http import HttpResponse
 
 # Create your views here.
-def home(request):
-    posteos = Entrada.objects.all()
-    return render(request, "bienvenida.html", {'posteos': posteos})
 
+#Eliminar este de testeos antes de entregar el trabajo
 def test(request):
     #posteos = Entrada.objects.all()
     return render(request, "navbar.html") #{'posteos': posteos}
+
+
+
+def home(request):
+    posteos = Entrada.objects.all()
+    return render(request, "bienvenida.html", {'posteos': posteos})
 
 def crearPost(request):
     form = EntradaForm()
@@ -22,3 +28,19 @@ def crearPost(request):
 
     context ={'form': form}
     return render(request, "crearpost.html", context)
+
+
+def buscar(request):
+    if request.GET["titulo"]:
+        queryset = request.GET["titulo"]       
+        entradas = Entrada.objects.filter(titulo__icontains=queryset).all()
+
+        if entradas!=[]:
+            return render(request, "busqueda_entrada.html", {"entradas": entradas, "titulo": queryset})
+
+    else:
+        respuesta = "Ha habido un error. Intente nuevamente."
+        return render(request, "busqueda_entrada.html", {"respuesta": respuesta})
+    
+    
+    
